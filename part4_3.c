afaaeba8d42b550c85ee1dddb2948508a6a95416
doriangrayspicture <140300007+doriangrayspicture@users.noreply.github.com>
2023-07-23 20:56:24 +0530

    Create part4_3.c

---
+++ part4_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fact(int a) {
+    if (a <= 1) {
+        return 1;
+    } else {
+        return a * fact(a - 1);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("%d\n", fact(n));
+
+    return 0;
+}
